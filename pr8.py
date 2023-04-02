3c8bdf0b630dc1f3fd2b98b8e43e47d14c70661f
Mobin Mohaddes <129539171+Itsmmob@users.noreply.github.com>
2023-04-02 21:46:46 +0330

    Add files via upload

---
+++ pr8.py
@@ -0,0 +1,13 @@
+class MyMedia: 
+	def Play (self,input): 
+		if input == "music": 
+			print("Playing a music")
+
+		elif input == "movie"
+			print("Showing cover of the movie")
+
+		elif input == "book"
+			print("A book is printing ")
+
+		else:
+			print("Lotfan yek pasokh sahih vared namaid")
